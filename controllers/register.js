68d464e8cbc020ee01a5e29cd591514f068ba154
Aksachlisimo <simo_akk@hotmail.com>
2024-11-18 16:01:33 +0100

    done

--- controllers/register.js
+++ controllers/register.js
@@ -1,40 +1,49 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
+
+  // Log the incoming request to verify data
+  console.log('Request body:', req.body);
+
+  // Validate input fields
   if (!email || !name || !password) {
-    return res.status(400).json('incorrect form submission');
+    console.log('Missing fields:', { email, name, password });
+    return res.status(400).json('Incorrect form submission');
   }
+
   const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
-        hash: hash,
-        email: email
-      })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-            // loginEmail[0] --> this used to return the email
-            // TO
-            // loginEmail[0].email --> this now returns the email
-            email: loginEmail[0].email,
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
+
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
     })
-    .catch(err => res.status(400).json('unable to register'))
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+      return trx('users')
+        .returning('*')
+        .insert({
+          email: loginEmail[0].email,
+          name: name,
+          joined: new Date()
+        })
+        .then(user => {
+          res.json(user[0]);
+        });
+    })
+    .then(trx.commit)
+    .catch(err => {
+      console.error('Transaction Error:', err);
+      trx.rollback();
+      res.status(400).json('Unable to register');
+    });
+  })
+  .catch(err => {
+    console.error('Transaction Setup Error:', err);
+    res.status(400).json('Unable to register');
+  });
 }
 
 module.exports = {
   handleRegister: handleRegister
 };
-
-
